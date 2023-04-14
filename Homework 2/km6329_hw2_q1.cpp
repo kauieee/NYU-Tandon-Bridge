1d16e4f8cbeb91d950e872a785c5f2733bee1a24
Kaushik <kaushik.manivannan@gmail.com>
2023-04-15 00:09:06 +0530

    Added HW2 Files

--- Homework 2/km6329_hw2_q1.cpp
+++ Homework 2/km6329_hw2_q1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int CENTS_IN_A_QUARTER = 25;
--- Homework 2/km6329_hw2_q3.cpp
+++ Homework 2/km6329_hw2_q3.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int HOURS_IN_A_DAY = 24;
